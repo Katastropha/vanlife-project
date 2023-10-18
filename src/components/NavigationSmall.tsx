5420dac722411ddf8e29e52d742336f9adde361c
Katastropha <weissjulia1991@gmail.com>
2023-10-18 14:54:07 +0200

    add styles for Host-Dashboard

--- src/components/NavigationSmall.tsx
+++ src/components/NavigationSmall.tsx
@@ -4,36 +4,16 @@ import { NavLink } from 'react-router-dom'
 export const NavigationSmall = (): ReactElement => {
   return (
     <div className="navigation-small">
-      <NavLink
-        to="/host"
-        className={(isActive): string => {
-          return `navigation-small__option ${!isActive && 'active'}`
-        }}
-      >
+      <NavLink to="/host" className="navigation-small__option">
         Dashboard
       </NavLink>
-      <NavLink
-        to="/host/income"
-        className={(isActive): string => {
-          return `navigation-small__option ${!isActive && 'active'}`
-        }}
-      >
+      <NavLink to="/host/income" className="navigation-small__option">
         Income
       </NavLink>
-      <NavLink
-        to="/host/hostVans"
-        className={(isActive): string => {
-          return `navigation-small__option ${!isActive && 'active'}`
-        }}
-      >
+      <NavLink to="/host/hostVans" className="navigation-small__option">
         Vans
       </NavLink>
-      <NavLink
-        to="/host/reviews"
-        className={(isActive): string => {
-          return `navigation-small__option ${!isActive && 'active'}`
-        }}
-      >
+      <NavLink to="/host/reviews" className="navigation-small__option">
         Reviews
       </NavLink>
     </div>
