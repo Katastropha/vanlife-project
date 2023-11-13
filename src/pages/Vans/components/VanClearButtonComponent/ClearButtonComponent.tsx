9f0e98b5fe5945c6915286df62b39e07bcdcaae1
Katastropha <weissjulia1991@gmail.com>
2023-11-13 14:27:31 +0100

    change vans component clear btn;

---
+++ src/pages/Vans/components/VanClearButtonComponent/ClearButtonComponent.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react'
+import styles from './ClearButtonComponent.module.css'
+
+type Props = {
+  handleFilter: () => void
+  text: string
+}
+export const ClearButtonComponent = ({
+  handleFilter,
+  text,
+}: Props): ReactElement => (
+  <button onClick={handleFilter} className={styles['van-filter-btn-clear']}>
+    {text}
+  </button>
+)
